6d50ea60fc09920954432f062f1dff522c30e78a
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-04 15:10:57 -0400

    create config variable to store google credentials

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
+const fs = require("node:fs");
+const path = require("node:path");
+const https = require("node:https");
 const express = require("express");
 const helmet = require("helmet");
 
+require("dotenv").config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 
